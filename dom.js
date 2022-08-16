296c29df7a8fb9523153ca7df05c8c5474fa15fd
Gowri239 <gowrimopuru14@gmail.com>
2022-08-16 16:26:31 +0530

    li change

--- dom.js
+++ dom.js
@@ -1,13 +1,2 @@
-console.log(document.domain);
-console.log(document.title);
-document.title=123;
-console.log(document.all);
-var header=document.getElementById('main-header');
-header.style.borderBottom='solid 3px #000';
-var additems = document.getElementsByClassName('title');
-additems[0].style.color='green';
-var items = document.getElementsByClassName('list-group-item');
-items[2].style.color='green';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
+var li = document.getElementsByTagName('li');
+li[4].style.color='green';
