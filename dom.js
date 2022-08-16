632291127bdee97ee6d41e25558164ea3c7af13d
Gowri239 <gowrimopuru14@gmail.com>
2022-08-16 16:01:24 +0530

    items change

--- dom.js
+++ dom.js
@@ -6,3 +6,8 @@ var header=document.getElementById('main-header');
 header.style.borderBottom='solid 3px #000';
 var additems = document.getElementsByClassName('title');
 additems[0].style.color='green';
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.color='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
