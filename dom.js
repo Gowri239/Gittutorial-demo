3b02d45206b0a00183bd2576ac51a998449f1858
Gowri239 <gowrimopuru14@gmail.com>
2022-08-16 17:07:01 +0530

    qs learn

--- dom.js
+++ dom.js
@@ -1,2 +1,9 @@
-var li = document.getElementsByTagName('li');
-li[4].style.color='green';
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display='none';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
